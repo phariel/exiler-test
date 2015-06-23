8466e08996684a09f1d9fc6db702a999cb53686f
Phil Peng <phil.peng@ef.com>
2015-06-23 11:40:05 +0800

    update

--- test.js
+++ test.js
@@ -5,12 +5,29 @@ var options = {
 		index: {
 			ex_data: {
 				aaa: 1
+			}
+		},
+		pageAction: {
+			ex_data: {
+				pageData: "I'm the page"
 			},
-			pageAction: {
-				ex_data: {
-					pageData: "I'm the page"
+			ex_template: "page.ejs"
+		},
+		ex_param_id: {
+			ex_data: function (param) {
+				return {
+					id: param.id
+				}
+			},
+			ex_template: "id.ejs",
+			ex_param_secondId: {
+				ex_data: function (param) {
+					return {
+						id: param.id,
+						secondId: param.secondId
+					}
 				},
-				ex_template: "page.ejs"
+				ex_template: "id.ejs"
 			}
 		}
 	}
