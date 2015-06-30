bc4001815f3eae7d1b87a3ce09a600ae60c1b1a2
Phil Peng <phil.peng@ef.com>
2015-06-30 18:47:29 +0800

    for v0.5.0

--- test.js
+++ test.js
@@ -3,8 +3,8 @@ var exiler = require("exiler");
 var options = {
 	route: {
 		index: {
-			ex_data: {
-				aaa: 1
+			ex_data: function (resolve) {
+				resolve({aaa: 1});
 			}
 		},
 		pageAction: {
@@ -14,18 +14,18 @@ var options = {
 			ex_template: "page.ejs"
 		},
 		ex_param_id: {
-			ex_data: function (param) {
-				return {
+			ex_data: function (resolve, param) {
+				resolve({
 					id: param.id
-				}
+				});
 			},
 			ex_template: "id.ejs",
 			ex_param_secondId: {
-				ex_data: function (param) {
-					return {
+				ex_data: function (resolve, param) {
+					resolve({
 						id: param.id,
 						secondId: param.secondId
-					}
+					});
 				},
 				ex_template: "id.ejs"
 			}
